2d3bd1ce0550dc836022daa25bec3f81f682b07c
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-05-20 08:34:54 -0400

    Add project files.

---
+++ Tarea_1/Entidades/Calculador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarea_1.Entidades
+{
+    public class Calculador
+    {
+        [Required(ErrorMessage = "No debe de Permanecer Vacío")]
+        public int Numero1 { get; set; }
+
+        [Required(ErrorMessage = "No debe de Permanecer Vacío")]
+        public int Numero2 { get; set; }
+
+        public int Suma { get; set; }
+        public int Resta { get; set; }
+        public int Multiplicacion { get; set; }
+
+        public Calculador()
+        {
+            Numero1 = 0;
+            Numero2 = 0;
+            Suma = 0;
+            Resta = 0;
+            Multiplicacion = 0;
+        }
+    }
+}
